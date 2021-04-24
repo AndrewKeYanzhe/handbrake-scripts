264543642eff357ea105e1ab4267801d26ab0293
Andrew <keyanzhe@gmail.com>
2021-04-24 16:02:11 +0800

    Added handbrakePath variable

--- upscale_to_8K.bat
+++ upscale_to_8K.bat
@@ -1,4 +1,6 @@
 set output_folder_name=Output
+set handbrakePath=E:Tools\HandBrakeCLI\HandBrakeCLI.exe
+
 mkdir "%~dp1\%output_folder_name%"
-E:Tools\HandBrakeCLI\HandBrakeCLI.exe -i %1 -o "%~dp1\%output_folder_name%\%~nx1" -e x264 -q 18 --width 7680 --height 4320 --encoder-preset ultrafast --aencoder copy
+%handbrakePath% -i %1 -o "%~dp1\%output_folder_name%\%~nx1" -e x264 -q 18 --width 7680 --height 4320 --encoder-preset ultrafast --aencoder copy
 pause
