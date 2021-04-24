ca08b45bf193228bc00c5028e4ee4178f1a82c97
Andrew <keyanzhe@gmail.com>
2021-04-24 18:56:25 +0800

    Disable auto crop

--- upscale_to_8K.bat
+++ upscale_to_8K.bat
@@ -12,5 +12,5 @@ rmdir /s /q "%~dp1\%output_folder_name%"
 mkdir "%~dp1\%output_folder_name%"
 
 
-%handbrakePath% -i %1 -o "%~dp1\%output_folder_name%\%~nx1" -e x264 -q 18 --width 7680 --height 4320 --encoder-preset ultrafast --aencoder copy
+%handbrakePath% -i %1 -o "%~dp1\%output_folder_name%\%~nx1" -e x264 -q 18 --width 7680 --height 4320 --encoder-preset ultrafast --aencoder copy --crop 0:0:0:0
 pause
