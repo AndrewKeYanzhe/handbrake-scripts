94d1a2ba78cff36ea2668b910e704e3b10167720
Andrew <keyanzhe@gmail.com>
2021-04-24 14:40:48 +0800

    Release Candidate

--- Archive/run2.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem echo %1 
-
-python run.py %1
-pause
--- upscale_to_8K.bat
+++ upscale_to_8K.bat
@@ -1,3 +1,4 @@
-@echo off
-pushd  %~dp0
-python upscale_to_8K.py %1
+set a=Output
+mkdir "%~dp1\%a%"
+E:Tools\HandBrakeCLI\HandBrakeCLI.exe -i %1 -o "%~dp1\%a%\%~nx1" -e x264 -q 18 -B 320 --width 7680 --height 4320 --encoder-preset ultrafast
+pause
