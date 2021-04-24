01442b5182c194d9fe44335c1acefe4e7e714454
Andrew <keyanzhe@gmail.com>
2021-04-24 14:57:03 +0800

    Switched to passthrough audio

--- upscale_to_8K.bat
+++ upscale_to_8K.bat
@@ -1,4 +1,4 @@
-set a=Output
-mkdir "%~dp1\%a%"
-E:Tools\HandBrakeCLI\HandBrakeCLI.exe -i %1 -o "%~dp1\%a%\%~nx1" -e x264 -q 18 -B 320 --width 7680 --height 4320 --encoder-preset ultrafast
+set output_folder_name=Output
+mkdir "%~dp1\%output_folder_name%"
+E:Tools\HandBrakeCLI\HandBrakeCLI.exe -i %1 -o "%~dp1\%output_folder_name%\%~nx1" -e x264 -q 18 --width 7680 --height 4320 --encoder-preset ultrafast --aencoder copy
 pause
