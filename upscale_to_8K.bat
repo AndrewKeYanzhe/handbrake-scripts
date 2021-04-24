5cb72f5fbed2ecdf21bda11a9ad895acbf49c91f
Andrew <keyanzhe@gmail.com>
2021-04-24 21:20:19 +0800

    Update comments

--- upscale_to_8K.bat
+++ upscale_to_8K.bat
@@ -1,6 +1,6 @@
-#drag video file on to this .bat file to resize
-#new video is saved in a folder next to original video
-#output folder is emptied before writing new video
+rem drag video file on to this .bat file to resize
+rem new video is saved in a folder next to original video
+rem output folder is emptied before writing new video
 
 
 set output_folder_name=Output
@@ -8,8 +8,8 @@ set handbrakePath=E:Tools\HandBrakeCLI\HandBrakeCLI.exe
 
 
 mkdir "%~dp1\%output_folder_name%"
-rmdir /s /q "%~dp1\%output_folder_name%"
-mkdir "%~dp1\%output_folder_name%"
+rem rmdir /s /q "%~dp1\%output_folder_name%"
+rem mkdir "%~dp1\%output_folder_name%"
 
 
 %handbrakePath% -i %1 -o "%~dp1\%output_folder_name%\%~nx1" -e x264 -q 18 --width 7680 --height 4320 --encoder-preset ultrafast --aencoder copy --crop 0:0:0:0
