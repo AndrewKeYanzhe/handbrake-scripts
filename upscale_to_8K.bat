173df0a262de435fac57713800190042a1e3f1e7
Andrew <keyanzhe@gmail.com>
2021-04-24 21:30:58 +0800

    Enabled encoding multiple videos

--- upscale_to_8K.bat
+++ upscale_to_8K.bat
@@ -1,16 +1,36 @@
 rem drag video file on to this .bat file to resize
 rem new video is saved in a folder next to original video
-rem output folder is emptied before writing new video
-
+@ECHO OFF
 
 set output_folder_name=Output
 set handbrakePath=E:Tools\HandBrakeCLI\HandBrakeCLI.exe
 
-
 mkdir "%~dp1\%output_folder_name%"
-rem rmdir /s /q "%~dp1\%output_folder_name%"
-rem mkdir "%~dp1\%output_folder_name%"
 
+rem this empties the output folder before writing new videos
+rem rmdir /s /q "%~dp1\%output_folder_name%"
+rem mkdir "%~dp1\%output_folder_name%"  dfdf
+
+:TOP
+IF (%1) == () GOTO END
 
 %handbrakePath% -i %1 -o "%~dp1\%output_folder_name%\%~nx1" -e x264 -q 18 --width 7680 --height 4320 --encoder-preset ultrafast --aencoder copy --crop 0:0:0:0
+
+SHIFT
+GOTO TOP
+
+:END
 pause
+
+
+
+
+
+
+
+
+
+
+
+
+
