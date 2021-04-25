083a66426501eef6d3661aed951b11681daa137a
Andrew <keyanzhe@gmail.com>
2021-04-25 18:17:31 +0800

    Update vp9 avc preference

--- youtubedl_download.bat
+++ youtubedl_download.bat
@@ -4,5 +4,5 @@ rem url_list can be for videos and playlists
 rem 1080p: av1>avc>vp9
 rem  720p: av1>vp9>avc
 
-youtube-dl -o "%~dp1%%(title)s" -f "bestvideo[height>=1080][vcodec^=av01]+bestaudio/bestvideo[height>=1080][vcodec^=avc1]+bestaudio/bestvideo[height>=1080][vcodec=vp9]+bestaudio/bestvideo[height>=720][vcodec^=av01]+bestaudio/bestvideo[height>=720][vcodec=vp9]+bestaudio/bestvideo[height>=720][vcodec^=avc1]+bestaudio/" --batch-file %1
+youtube-dl -o "%~dp1%%(title)s" -f "bestvideo[height>=1080][vcodec^=av01]+bestaudio/bestvideo[height>=1080][vcodec^=avc1][vbr>=2850]+bestaudio/bestvideo[height>=1080][vcodec=vp9]+bestaudio/bestvideo[height>=1080][vcodec^=avc1]+bestaudio/bestvideo[height>=720][vcodec^=av01]+bestaudio/bestvideo[height>=720][vcodec=vp9]+bestaudio/bestvideo[height>=720][vcodec^=avc1]+bestaudio/" --batch-file %1
 pause
