c1b7d449ec14f159145ff08efd01602803e81c48
Andrew <keyanzhe@gmail.com>
2021-04-25 21:19:43 +0800

    add notes

--- youtube-dl/download.bat
+++ youtube-dl/download.bat
@@ -7,7 +7,8 @@ rem 1080p24
 rem avc 2.91>vp9 2.08,71.4
 
 rem 1080p30
-rem avc 3.61>vp2.65,73.4
+rem avc 3.61>vp9 2.65,73.4
+rem avc 3.63>vp9 16.5,45.4
 
 rem 1080p60
 rem avc 2.73<vp9 2.24,82.1
@@ -37,4 +38,4 @@ bestvideo[height^>=720][vcodec=vp9]+bestaudio/^
 bestvideo[height^>=720][vcodec^^=avc1]+bestaudio
 
 youtube-dl -o "%~dp1%%(title)s" -f "%formats%" --batch-file %1
-rem pause
+pause
