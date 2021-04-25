7309081b9c88c11033690bdf2268d8fa1a687109
Andrew <keyanzhe@gmail.com>
2021-04-26 00:09:38 +0800

    docs: remove codec quality notes

--- youtube-dl/download.bat
+++ youtube-dl/download.bat
@@ -1,28 +1,9 @@
 rem drag url_listtxt onto this bat file to run
 rem url_list can be for videos and playlists
 
-rem ^ needs to be written as ^^, > needs to be written as ^>, ^is used for multi-line
-
-rem 1080p24
-rem 2800
-rem avc 2646>vp9 2460,	93.0
-rem avc 3090>vp9 2650,	85.8
-rem avc 2910>vp9 2080,	71.4
-rem avc 1800>vp9 0680,	37.8
-
-
-rem 1080p30
-rem 2900
-rem avc 2990>vp9 2650,	88.5
-rem avc 3610>vp9 2650,	73.4
-rem avc 3630>vp9 1650,	45.4
-rem avc 2610>vp9 0980,	37.7
-
-
-rem 1080p60
-rem avc 2730<vp9 2240,	82.1
-rem avc 3850<vp9 3040,	79.0
-
+rem ^^ is ^
+rem ^> is >
+rem ^ is for multi-line
 
 set formats=^
 bestvideo[height^>=1080][fps^>30][vcodec^^=av01]+bestaudio/^
