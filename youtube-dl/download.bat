0aab2be3ac4b11a0613f0e04d28b8fa4155be758
Andrew <keyanzhe@gmail.com>
2021-04-26 00:17:09 +0800

    feat: download video in all 3 codecs

--- youtube-dl/download.bat
+++ youtube-dl/download.bat
@@ -1,31 +1,24 @@
 rem drag url_listtxt onto this bat file to run
 rem url_list can be for videos and playlists
+rem video is downloaded in all 3 codecs if available
 
 rem ^^ is ^
 rem ^> is >
 rem ^ is for multi-line
 
-set formats=^
-bestvideo[height^>=1080][fps^>30][vcodec^^=av01]+bestaudio/^
-bestvideo[height^>=1080][fps^>30][vcodec^^=vp9]+bestaudio/^
-bestvideo[height^>=1080][fps^>30][vcodec^^=avc]+bestaudio/^
-bestvideo[height^>=1080][fps=30][vcodec^^=av01]+bestaudio/^
-bestvideo[height^>=1080][fps=30][vcodec^^=avc1][vbr^>=2600]+bestaudio/^
-bestvideo[height^>=1080][fps=30][vcodec=vp9]+bestaudio/^
-bestvideo[height^>=1080][fps=30][vcodec^^=avc1]+bestaudio/^
-bestvideo[height^>=1080][fps^>24][vcodec^^=av01]+bestaudio/^
-bestvideo[height^>=1080][fps^>24][vcodec^^=vp9]+bestaudio/^
-bestvideo[height^>=1080][fps^>24][vcodec^^=avc]+bestaudio/^
-bestvideo[height^>=1080][fps=24][vcodec^^=av01]+bestaudio/^
-bestvideo[height^>=1080][fps=24][vcodec^^=avc1][vbr^>=2600]+bestaudio/^
-bestvideo[height^>=1080][fps=24][vcodec=vp9]+bestaudio/^
-bestvideo[height^>=1080][fps=24][vcodec^^=avc1]+bestaudio/^
-bestvideo[height^>=1080][fps^<24][vcodec^^=av01]+bestaudio/^
-bestvideo[height^>=1080][fps^<24][vcodec^^=vp9]+bestaudio/^
-bestvideo[height^>=1080][fps^<24][vcodec^^=avc]+bestaudio/^
-bestvideo[height^>=720][vcodec^^=av01]+bestaudio/^
-bestvideo[height^>=720][vcodec=vp9]+bestaudio/^
-bestvideo[height^>=720][vcodec^^=avc1]+bestaudio
+set prefer_av1=^
+bestvideo[vcodec^^=av01]+bestaudio/^
+bestvideo[vcodec^^=vp9]+bestaudio/^
+bestvideo[vcodec^^=avc]+bestaudio
 
-youtube-dl -o "%~dp1%%(title)s %%(vcodec)s" -f "%formats%" --batch-file %1
+set prefer_vp9=^
+bestvideo[vcodec^^=vp9]+bestaudio/^
+bestvideo[vcodec^^=avc]+bestaudio
+
+set prefer_avc=^
+bestvideo[vcodec^^=avc]+bestaudio
+
+youtube-dl -o "%~dp1%%(title)s %%(vcodec)s" -f "%prefer_av1%" --batch-file %1
+youtube-dl -o "%~dp1%%(title)s %%(vcodec)s" -f "%prefer_vp9%" --batch-file %1
+youtube-dl -o "%~dp1%%(title)s %%(vcodec)s" -f "%prefer_avc%" --batch-file %1
 pause
