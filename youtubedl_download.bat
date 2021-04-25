e37c4bb13bfd901bb7b66be4c6e27fd1f8c7dea5
Andrew <keyanzhe@gmail.com>
2021-04-25 09:05:00 +0800

    Updated youtube-dl instructions

--- youtubedl_download.bat
+++ youtubedl_download.bat
@@ -1,4 +1,5 @@
 rem drag url_list.txt onto this .bat file to run
+rem url_list can be for videos and playlists
 echo %~dp1
 
 youtube-dl -o "%~dp1%%(title)s" -f 399+bestaudio/137+bestaudio/248+bestaudio --batch-file %1
