29c7c4dfca08bfdd49bba7fb66c998b680583d65
Andrew <keyanzhe@gmail.com>
2021-04-25 18:46:26 +0800

    switch to multi-line codec preferences

--- youtubedl_download.bat
+++ youtubedl_download.bat
@@ -1,8 +1,18 @@
 rem drag url_list.txt onto this .bat file to run
 rem url_list can be for videos and playlists
 
-rem 1080p: av1>avc>vp9
-rem  720p: av1>vp9>avc
+rem 1080p: av1^>avc^>vp9
+rem  720p: av1^>vp9^>avc
 
-youtube-dl -o "%~dp1%%(title)s" -f "bestvideo[height>=1080][vcodec^=av01]+bestaudio/bestvideo[height>=1080][vcodec^=avc1][vbr>=2850]+bestaudio/bestvideo[height>=1080][vcodec=vp9]+bestaudio/bestvideo[height>=1080][vcodec^=avc1]+bestaudio/bestvideo[height>=720][vcodec^=av01]+bestaudio/bestvideo[height>=720][vcodec=vp9]+bestaudio/bestvideo[height>=720][vcodec^=avc1]+bestaudio/" --batch-file %1
+
+set formats=^
+bestvideo[height^>=1080][vcodec^^=av01]+bestaudio/^
+bestvideo[height^>=1080][vcodec^^=avc1][vbr^>=2850]+bestaudio/^
+bestvideo[height^>=1080][vcodec=vp9]+bestaudio/^
+bestvideo[height^>=1080][vcodec^^=avc1]+bestaudio/^
+bestvideo[height^>=720][vcodec^^=av01]+bestaudio/^
+bestvideo[height^>=720][vcodec=vp9]+bestaudio/^
+bestvideo[height^>=720][vcodec^^=avc1]+bestaudio
+
+youtube-dl -o "%~dp1%%(title)s" -f "%formats%" --batch-file %1
 pause
