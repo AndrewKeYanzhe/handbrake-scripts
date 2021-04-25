ab6816d60636be615cd48cef2020ebdccd3e2672
Andrew <keyanzhe@gmail.com>
2021-04-25 09:57:17 +0800

    Enabled pasting URL into CMD

--- youtubedl_list_formats.bat
+++ youtubedl_list_formats.bat
@@ -1,3 +1,15 @@
 rem drag url_list.txt onto this .bat file to run
-youtube-dl --list-formats --batch-file %1
-pause
+
+
+
+
+if NOT "%~1"=="" youtube-dl --list-formats --batch-file %1
+if NOT "%~1"=="" pause
+
+
+:START
+set /P urls=Enter URL
+echo %urls%
+youtube-dl --list-formats %urls%
+
+if "%~1"=="" GOTO START
