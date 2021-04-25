9b26fe99aed5a4d75aab7bb4ca3349a05cf9fcbe
Andrew <keyanzhe@gmail.com>
2021-04-25 21:02:42 +0800

    Update 30fps codec preferences

--- youtubedl_download.bat
+++ youtubedl_download.bat
@@ -3,13 +3,30 @@ rem url_list can be for videos and playlists
 
 rem ^ needs to be written as ^^, > needs to be written as ^>, ^is used for multi-line
 
+rem 1080p24
+rem avc 2.91>vp9 2.08,71.4
+
+rem 1080p30
+rem avc 3.61>vp2.65,73.4
+
+rem 1080p60
+rem avc 2.73<vp9 2.24,82.1
+rem avc 3.85<vp9 3.04,79.0
+
 
 set formats=^
+bestvideo[height^>=1080][fps^>30][vcodec^^=av01]+bestaudio/^
+bestvideo[height^>=1080][fps^>30][vcodec^^=vp9]+bestaudio/^
+bestvideo[height^>=1080][fps^>30][vcodec^^=avc]+bestaudio/^
+bestvideo[height^>=1080][fps=30][vcodec^^=av01]+bestaudio/^
+bestvideo[height^>=1080][fps=30][vcodec^^=avc1][vbr^>=3500]+bestaudio/^
+bestvideo[height^>=1080][fps=30][vcodec=vp9]+bestaudio/^
+bestvideo[height^>=1080][fps=30][vcodec^^=avc1]+bestaudio/^
 bestvideo[height^>=1080][fps^>24][vcodec^^=av01]+bestaudio/^
 bestvideo[height^>=1080][fps^>24][vcodec^^=vp9]+bestaudio/^
 bestvideo[height^>=1080][fps^>24][vcodec^^=avc]+bestaudio/^
 bestvideo[height^>=1080][fps=24][vcodec^^=av01]+bestaudio/^
-bestvideo[height^>=1080][fps=24][vcodec^^=avc1][vbr^>=2850]+bestaudio/^
+bestvideo[height^>=1080][fps=24][vcodec^^=avc1][vbr^>=2800]+bestaudio/^
 bestvideo[height^>=1080][fps=24][vcodec=vp9]+bestaudio/^
 bestvideo[height^>=1080][fps=24][vcodec^^=avc1]+bestaudio/^
 bestvideo[height^>=1080][fps^<24][vcodec^^=av01]+bestaudio/^
@@ -20,4 +37,4 @@ bestvideo[height^>=720][vcodec=vp9]+bestaudio/^
 bestvideo[height^>=720][vcodec^^=avc1]+bestaudio
 
 youtube-dl -o "%~dp1%%(title)s" -f "%formats%" --batch-file %1
-pause
+rem pause
