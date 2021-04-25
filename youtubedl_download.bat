af0ed8408ed5fdb56e5895d753c031ed501840e7
Andrew <keyanzhe@gmail.com>
2021-04-25 15:29:09 +0800

    Add 720p codec preferences

--- youtubedl_download.bat
+++ youtubedl_download.bat
@@ -1,6 +1,8 @@
 rem drag url_list.txt onto this .bat file to run
 rem url_list can be for videos and playlists
-echo %~dp1
 
-youtube-dl -o "%~dp1%%(title)s" -f 399+bestaudio/137+bestaudio/248+bestaudio --batch-file %1
+rem 1080p: 399,137,238: av1,avc,vp9
+rem  720p: 398,247,136: av1,vp9,avc
+
+youtube-dl -o "%~dp1%%(title)s" -f 399+bestaudio/137+bestaudio/248+bestaudio/398+bestaudio/247+bestaudio/136+bestaudio --batch-file %1
 pause
