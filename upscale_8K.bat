48685bc4b878608efd905929c1e152d27b941d9d
Andrew <keyanzhe@gmail.com>
2021-04-26 18:44:47 +0800

    feat: open folder with upscale_8K.bat
    upscale_8K.bat iterates through every file in the folder and encodes them

--- upscale_8K.bat
+++ upscale_8K.bat
@@ -1,5 +1,3 @@
-rem drag video file on to this .bat file to resize
-rem new video is saved in a folder next to original video
 @ECHO OFF
 
 set output_folder_name=Output
@@ -7,30 +5,13 @@ set handbrakePath=E:Tools\HandBrakeCLI\HandBrakeCLI.exe
 
 mkdir "%~dp1\%output_folder_name%"
 
-rem this empties the output folder before writing new videos
-rem rmdir /s /q "%~dp1\%output_folder_name%"
-rem mkdir "%~dp1\%output_folder_name%"  dfdf
 
-:TOP
-IF (%1) == () GOTO END
+FOR %%i IN (%1\*.*) DO (
+	echo %%i
 
-%handbrakePath% -i %1 -o "%~dp1\%output_folder_name%\%~n1.mkv" -e x264 -q 18 --width 7680 --height 4320 --encoder-preset ultrafast --aencoder copy --crop 0:0:0:0
 
-SHIFT
-GOTO TOP
+	%handbrakePath% -i "%%i" -o "%~dp1\%output_folder_name%\%%~ni.mkv" -e x264 -q 18 --width 7680 --height 4320 --encoder-preset ultrafast --aencoder copy --crop 0:0:0:0
+	del "%%i"
+)
 
-:END
 pause
-
-
-
-
-
-
-
-
-
-
-
-
-
