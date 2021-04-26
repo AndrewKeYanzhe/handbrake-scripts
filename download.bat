57a9235416c5ea01461430c7514f8f81820566ba
Andrew <keyanzhe@gmail.com>
2021-04-26 10:25:33 +0800

    feat: specify download folder

--- download.bat
+++ download.bat
@@ -6,6 +6,11 @@ rem ^^ is ^
 rem ^> is >
 rem ^ is for multi-line
 
+set download_folder_name=Download
+echo %~dp1%download_folder_name%\
+set download_path=%~dp1%download_folder_name%\
+rem pause
+
 set prefer_av1=^
 bestvideo[vcodec^^=av01]+bestaudio/^
 bestvideo[vcodec^^=vp9]+bestaudio/^
@@ -18,7 +23,7 @@ bestvideo[vcodec^^=avc]+bestaudio
 set prefer_avc=^
 bestvideo[vcodec^^=avc]+bestaudio
 
-youtube-dl -o "%~dp1%%(title)s %%(vcodec)s" -f "%prefer_av1%" --batch-file %1
-youtube-dl -o "%~dp1%%(title)s %%(vcodec)s" -f "%prefer_vp9%" --batch-file %1
-youtube-dl -o "%~dp1%%(title)s %%(vcodec)s" -f "%prefer_avc%" --batch-file %1
+youtube-dl -o "%download_path%%%(title)s codec=%%(vcodec)s" -f "%prefer_av1%" --batch-file %1
+youtube-dl -o "%download_path%%%(title)s codec=%%(vcodec)s" -f "%prefer_vp9%" --batch-file %1
+youtube-dl -o "%download_path%%%(title)s codec=%%(vcodec)s" -f "%prefer_avc%" --batch-file %1
 pause
