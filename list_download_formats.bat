3bcde30022b8f8024a57978cd42ef8ae041c2988
Andrew <keyanzhe@gmail.com>
2021-04-26 19:07:07 +0800

    refactor: list_download_formats.bat

--- list_download_formats.bat
+++ list_download_formats.bat
@@ -1,16 +1,21 @@
-rem drag url_list.txt onto this .bat file to run
+@ECHO OFF
+rem drag urls.txt onto this .bat file to run
 
 
+if NOT "%~1"=="" (
+	youtube-dl --get-title --batch-file %1
+	youtube-dl --list-formats --batch-file %1
+	pause
+	EXIT /B
+	)
 
 
-if NOT "%~1"=="" youtube-dl --get-title --batch-file %1
-if NOT "%~1"=="" youtube-dl --list-formats --batch-file %1
-if NOT "%~1"=="" pause
 
 
 :START
-set /P urls=Enter URL
+set /P urls=Enter URL 
 echo %urls%
+youtube-dl --get-title %urls%
 youtube-dl --list-formats %urls%
 
 if "%~1"=="" GOTO START
