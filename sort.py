cd79d7b77e2051b623581451e2422d055c160247
Andrew <keyanzhe@gmail.com>
2021-04-26 10:37:17 +0800

    feat: sort.py lists N files for each title

--- sort.py
+++ sort.py
@@ -4,23 +4,30 @@ import os
 print(sys.argv[1])
 directory = sys.argv[1]
 
-video_names=[]
+video_titles=[]
 
 for filename in os.listdir(directory):
     if filename.endswith(".mkv") or filename.endswith(".mp4") or filename.endswith(".webm"): 
         file_path = os.path.join(directory, filename)
+
         
         # print()
-        video_name = os.path.splitext(file_path)[0].split(" codec=")[0]
-        if video_name in video_names:
+        video_title = os.path.splitext(filename)[0].split(" codec=")[0]
+        if video_title in video_titles:
             continue
-        video_names.append(video_name)
-        print(video_name)
-        
+        video_titles.append(video_title)
+        print(video_title)
+
+        for file in os.listdir(directory):
+            if file.startswith(video_title):
+                # print("hi")
+                # print(os.path.join(directory, file))
+                print(file)
+
 
 
         continue
     else:
         continue
 
-# print(len(video_names))
+print(len(video_titles))
