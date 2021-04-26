ab577992fa7d99e34c59abfc5333e34cf1b8a28c
Andrew <keyanzhe@gmail.com>
2021-04-26 18:16:23 +0800

    feat: always encode 8K to .mkv
    webm cannot store the encoded 8K h264
    
    so, rather than (webm>webm), we use (all formats>mkv)

--- upscale_8K.bat
+++ upscale_8K.bat
@@ -14,7 +14,7 @@ rem mkdir "%~dp1\%output_folder_name%"  dfdf
 :TOP
 IF (%1) == () GOTO END
 
-%handbrakePath% -i %1 -o "%~dp1\%output_folder_name%\%~nx1" -e x264 -q 18 --width 7680 --height 4320 --encoder-preset ultrafast --aencoder copy --crop 0:0:0:0
+%handbrakePath% -i %1 -o "%~dp1\%output_folder_name%\%~n1.mkv" -e x264 -q 18 --width 7680 --height 4320 --encoder-preset ultrafast --aencoder copy --crop 0:0:0:0
 
 SHIFT
 GOTO TOP
