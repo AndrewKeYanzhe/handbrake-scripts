f63105b45187ba33357da453762fc13f2d9828fa
Andrew <keyanzhe@gmail.com>
2021-04-26 18:55:27 +0800

    docs: update download.bat

--- download.bat
+++ download.bat
@@ -1,15 +1,6 @@
-rem drag url_list.txt onto this bat file to run
-rem url_list can be for videos and playlists
-rem video is downloaded in all 3 codecs if available
-
-rem ^^ is ^
-rem ^> is >
-rem ^ is for multi-line
-
 set download_folder_name=Download
 echo %~dp1%download_folder_name%\
 set download_path=%~dp1%download_folder_name%\
-rem pause
 
 set prefer_av1=^
 bestvideo[vcodec^^=av01]+bestaudio/^
